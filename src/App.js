d8559016295a7eee38cbe799976ef3ed05cb48a6
tanukeshari <kesharitanu@gmail.com>
2023-04-27 18:52:55 +0530

    Signup Screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+
+import Header from "./components/Header/Header";
+import Home from "./components/Pages/Home";
+import AuthForm from "./components/Auth/AuthForm";
+
+const router = createBrowserRouter([
+  { 
+    path: "/", 
+    element: <Header />, 
+    children: [
+      { path: "/", element: <Home/> },
+      { path: "/authform", element: <AuthForm/>},
+    ], 
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToastContainer/>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
